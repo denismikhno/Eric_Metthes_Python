b7aa04fd053efae30bf93b108280e88c497a5d9d
denismikhno_work <denismikhno@gmail.com>
2025-06-23 13:01:44 +0300

    add 3_full_name.py

---
+++ 3_full_name.py
@@ -0,0 +1,13 @@
+first_name = "Ivan"
+last_name = "Ivanov"
+full_name = f"{first_name} {last_name}\n\n\n"
+print(full_name)
+print(f"\tHello, {first_name} {last_name}!")
+
+print("python      ".rstrip())
+print("     python".lstrip())
+
+url1 = 'https://yandex.ru'
+url2 = url1.removeprefix('https://')
+print(url1)
+print(url2)
